ebd3f4bd9f3fe118549337a3fd021164c8dc56ce
Wutpeach <wutpeach@gmail.com>
2026-01-28 18:58:25 +0800

    feat: invoke process_files and enhance visual

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -25,11 +25,16 @@ fn list_files(path: String) -> Result<Vec<String>, String> {
     Ok(files)
 }
 
+#[tauri::command]
+fn process_files(paths: Vec<String>) {
+    println!(">>> [Rust] Receiving files to process: {:?}", paths);
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![list_files])
+        .invoke_handler(tauri::generate_handler![list_files, process_files])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
